e8a93e101d320c574778b7ee5a39857b7fa45807
Ching-An Cheng <chinganc0@gmail.com>
2022-03-21 16:37:18 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@ setup(
                       'termcolor',  # adept_envs dependency
                       'click',  # adept_envs dependency
                       'dm_control==0.0.364896371',
-                      'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
+                       #'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
+                      'mjrl@git+https://github.com/aravindr93/mjrl.git@master#egg=mjrl']
     packages=find_packages(),
     package_data={'d4rl': ['locomotion/assets/*',
                            'hand_manipulation_suite/assets/*',
